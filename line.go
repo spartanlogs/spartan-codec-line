38de9cb4285ea99c28997cdee9b4657f0d16dd01
Lee Keitel <lee@keitel.xyz>
2017-05-06 21:16:45 -0500

    Accept options

--- line.go
+++ line.go
@@ -4,23 +4,52 @@ import (
 	"io"
 
 	"github.com/spartanlogs/spartan/codecs"
+	"github.com/spartanlogs/spartan/config"
 	"github.com/spartanlogs/spartan/event"
+	"github.com/spartanlogs/spartan/utils"
 )
 
+type lineConfig struct {
+	delimiter string
+}
+
+var lineConfigSchema = []config.Setting{
+	{
+		Name:    "delimiter",
+		Type:    config.String,
+		Default: "\n",
+	},
+}
+
 // The LineCodec reads plaintext with delimiter of \n
-type LineCodec struct{}
+type LineCodec struct {
+	config *lineConfig
+}
 
 func init() {
 	codecs.Register("line", newLineCodec)
 }
 
-func newLineCodec() (codecs.Codec, error) {
-	return &LineCodec{}, nil
+func newLineCodec(options utils.InterfaceMap) (codecs.Codec, error) {
+	c := &LineCodec{
+		config: &lineConfig{},
+	}
+	return c, c.setConfig(options)
+}
+
+func (c *LineCodec) setConfig(options utils.InterfaceMap) error {
+	if err := config.VerifySettings(options, lineConfigSchema); err != nil {
+		return err
+	}
+
+	c.config.delimiter = options.Get("delimiter").(string)
+
+	return nil
 }
 
 // Encode event as a simple message.
 func (c *LineCodec) Encode(e *event.Event) []byte {
-	return []byte(e.String() + "\n")
+	return []byte(e.String() + c.config.delimiter)
 }
 
 // EncodeWriter reads events from in and writes them to w
